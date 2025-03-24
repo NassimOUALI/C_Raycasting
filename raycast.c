96691fb0a759301ee17ce8d7791929f24731c2f2
Nimo <nassimouali484@gmail.com>
2025-03-24 14:38:27 +0000

    Added collision detection

--- raycast.c
+++ raycast.c
@@ -130,10 +130,22 @@ void display(){
     //fps
     frame2 = glutGet(GLUT_ELAPSED_TIME); fps=(frame2-frame1); frame1 = glutGet(GLUT_ELAPSED_TIME);
 
-    if (Keys.a == 1) { pa -= 0.1*fps; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa)); }
-    if (Keys.d == 1) { pa += 0.1*fps; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa));  }
-    if (Keys.w == 1) { px += pdx*0.1*fps; py += pdy*0.1*fps; }
-    if (Keys.s == 1) { px -= pdx*0.1*fps; py -= pdy*0.1*fps; }
+    if (Keys.d == 1) { pa -= 0.1*fps; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa)); }
+    if (Keys.a == 1) { pa += 0.1*fps; pa=FixAng(pa); pdx=cos(degToRad(pa)); pdy=-sin(degToRad(pa));  }
+
+    int xo=0; if(pdx<0){ xo=-20; } else {xo=20;}
+    int yo=0; if(pdy<0){ yo=-20; } else {yo=20;}
+    int ipx=px/64.0, ipx_add_xo=(px+xo)/64.0, ipx_sub_xo=(px-xo)/64.0;
+    int ipy=py/64.0, ipy_add_yo=(py+yo)/64.0, ipy_sub_yo=(py-yo)/64.0;
+
+    if (Keys.w == 1) {
+            if(map[ipy*mapX         + ipx_add_xo]==0){ px += pdx*0.1*fps;}
+            if(map[ipy_add_yo*mapX  + ipx       ]==0){ py += pdy*0.1*fps; }
+    }
+    if (Keys.w == 1) {
+            if(map[ipy*mapX         + ipx_sub_xo]==0){ px -= pdx*0.1*fps;}
+            if(map[ipy_sub_yo*mapX  + ipx       ]==0){ py -= pdy*0.1*fps; }
+    }
     glutPostRedisplay();
 
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
